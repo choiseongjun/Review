8861994a5ef5f91e7190f760bdd590f04161c9eb
choiseongjun <csj2702@naver.com>
2020-02-15 20:45:04 +0900

    로그인 수정

---
+++ src/main/resources/static/js/user/register.js
@@ -0,0 +1,36 @@
+
+$('#register_btn').off().on('click',function(){
+	console.log('sdfsdfsdf')
+	let signUpRequest = {};
+	signUpRequest.email=$('#email').val();
+	signUpRequest.nickname=$('#nickname').val();
+	signUpRequest.password=$('#password').val();
+	signUpRequest.sex=$('#sex').val();
+	signUpRequest.birth=$('#birth').val();
+
+	let formData = new FormData();
+//	formData.append("file", $('#people_image_file')[0].files[0]);
+//	formData.append('signUpRequest', new Blob([JSON.stringify(user)], {
+//	        type: "application/json; charset=UTF-8"
+//	    }));
+	$.ajax({
+		url:'/api/auth/signup',
+		type:'POST',
+		// enctype: 'multipart/form-data',
+	        dataType:'json',
+		//mimeType:"multipart/form-data",
+	    data: signUpRequest,
+		success:function(data){
+			if(data.code==1){
+				alert(data.message);
+				//location.href='/signinView';
+			}else{
+				alert(data.message);
+			}
+		},
+		error:function(){
+
+		}
+
+	});
+});
