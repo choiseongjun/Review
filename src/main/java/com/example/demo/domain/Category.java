d8515f1f34582bf9b87a2b474eba6d5005c386c5
inaeLee <inae9709@hanmail.net>
2020-04-28 01:57:17 +0900

    [Fix] Category 계층형 쿼리 수정 ResultMap 선언, Collection으로 서브 카테고리 가져오는 방식..

--- src/main/java/com/example/demo/domain/Category.java
+++ src/main/java/com/example/demo/domain/Category.java
@@ -1,23 +1,13 @@
 package com.example.demo.domain;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import com.example.demo.domain.common.DateAudit;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Category extends DateAudit{
+public class Category{
 	//카테고리 id
 	private Long id;
 	
@@ -26,4 +16,7 @@ public class Category extends DateAudit{
 	
 	//카테고리 parent
 	private Long parent;
+	
+	//List<Category>
+	private List<Category> subCategory;
 }
