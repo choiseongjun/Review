5d65e9678afda465b83020fce6a033ba23601532
inaeLee <inae9709@hanmail.net>
2020-05-13 03:18:19 +0900

    Add Category insert, update feature

--- src/main/java/com/example/demo/controller/category/CategoryController.java
+++ src/main/java/com/example/demo/controller/category/CategoryController.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.message.request.CategoryReq;
 import com.example.demo.service.category.CategoryService;
 
 
@@ -26,5 +31,23 @@ public class CategoryController {
 		}
 	}
 	
+	@PostMapping("/category")
+	public ResponseEntity<?> createCategory(@RequestBody CategoryReq categoryReq) {
+		try {
+			return new ResponseEntity<>(categoryService.createCategory(categoryReq), HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>("Internal Server ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@PutMapping("/category")
+	public ResponseEntity<?> editCategory(@RequestBody CategoryReq categoryReq) {
+		try {
+			return new ResponseEntity<>(categoryService.editCategroy(categoryReq), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>("Internal Server ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 }
--- src/main/java/com/example/demo/service/category/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/category/CategoryServiceImpl.java
@@ -1,12 +1,17 @@
 package com.example.demo.service.category;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Category;
+import com.example.demo.message.request.CategoryReq;
+import com.example.demo.message.response.ApiResponseMessage;
 import com.example.demo.repository.CategoryDao;
+import com.example.demo.repository.CategoryRepository;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -14,9 +19,36 @@ public class CategoryServiceImpl implements CategoryService {
 	@Autowired
 	private CategoryDao categoryDao;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public List<Category> selectCategory() {
 		return categoryDao.selectCategory();
 	}
+
+	@Override
+	public Category createCategory(CategoryReq categoryReq) {
+		Category category = new Category(categoryReq.getMCode(), categoryReq.getName(), categoryReq.getParent());
+		return categoryRepository.save(category);
+	}
+
+	@Override
+	public boolean editCategroy(CategoryReq categoryReq) {
+		try {
+			Optional<Category> category = categoryRepository.findById(categoryReq.getId());
+			
+			category.ifPresent(selectCategory -> {
+				selectCategory.setName(categoryReq.getName());
+				selectCategory.setMCode(categoryReq.getMCode());
+				selectCategory.setParent(categoryReq.getParent());
+				categoryRepository.save(selectCategory);
+			});
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
 	
 }
