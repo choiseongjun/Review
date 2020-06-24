ba6871921cef7929d3a5c5f514f8bfaba868751d
최성준 <csj2702@naver.com>
2020-06-25 00:18:36 +0900

    jpa 한국시간 설정

--- src/main/java/com/example/demo/ReviewApplication.java
+++ src/main/java/com/example/demo/ReviewApplication.java
@@ -1,11 +1,19 @@
 package com.example.demo;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReviewApplication {
 
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewApplication.class, args);
 	}
